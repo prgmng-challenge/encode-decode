2ee1a27b5212398338574f620af73b1df5e9c1a4
Joshua Rodgers-AR River <52717232+rodgersjaresc@users.noreply.github.com>
2020-03-03 15:02:03 -0600

    Support for numbers more punctuation and helpful errors.
    Added support for encoding numbers, commas, and single quotes. encode() returns helpful messages if input is too long or an empty string.

--- src/encoder.hpp
+++ src/encoder.hpp
@@ -5,19 +5,29 @@ using namespace std;
 
 int convert(char in)
 {
-    char lower[27] = {' ', 'a', 'b', 'c', 'd', 'e', 'f',
+    char lower[28] = {' ', 'a', 'b', 'c', 'd', 'e', 'f',
                         'g', 'h', 'i', 'j', 'k', 'l',
                         'm', 'n', 'o', 'p', 'q', 'r', 
                         's', 't', 'u', 'v', 'w', 'x', 
-                        'y', 'z'};
+                        'y', 'z', ','};
 
-    char upper[27] = {'.', 'A', 'B', 'C', 'D', 'E', 'F',
+    char upper[28] = {'.', 'A', 'B', 'C', 'D', 'E', 'F',
                         'G', 'H', 'I', 'J', 'K', 'L',
                         'M', 'N', 'O', 'P', 'Q', 'R',
                         'S', 'T', 'U', 'V', 'W', 'X', 
-                        'Y', 'Z'};
+                        'Y', 'Z', '\''};
 
-    for(int i = 0; i < 26; i++)
+    short nums[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+    for(int i = 0; i < 10; i++)
+    {
+        if(in == nums[i])
+        {
+            return nums[i];
+        }
+    }     
+
+    for(int i = 0; i < 28; i++)
     {
         if(in == lower[i])
         {
@@ -25,14 +35,14 @@ int convert(char in)
         }
     }
 
-    for(int i = 0; i < 26; i++)
+    for(int i = 0; i < 28; i++)
     {
         if(in == upper[i])
         {
             return i;
         }
     } 
-    return 27;
+    return 28;
 }
 
 string encode(string input)
@@ -43,30 +53,36 @@ string encode(string input)
 
     string output;
 
-    if(input.length() > 0)
+    if(input.length() > 50)
     {
-     
-        for(int i = 0; i < input.length(); i++)
-        {
-            converted[i] = convert(input[i]);
-            position++;
-        }
-
-        for(int i = position - 1, j = 0; i >= 0; i--, j++)
-        {
-            rotated[j] = converted[i];
-        } 
-
-        for(int i = position - 1; i >= 0; i--)
-        {
-            rotated[i] = rotated[i] * i;
-        }
-
-        for(int i = 0; i < input.length(); i++)
-        {
-            output += to_string(rotated[i]);
-        }
+        return "Input overflow.";
+    }else if(input.length() == 0)
+    {
+        return "No input received.";
     }
+
+     
+    for(int i = 0; i < input.length(); i++)
+    {
+        converted[i] = convert(input[i]);
+        position++;
+    }
+
+    for(int i = position - 1, j = 0; i >= 0; i--, j++)
+    {
+        rotated[j] = converted[i];
+    } 
+
+    for(int i = position - 1; i >= 0; i--)
+    {
+        rotated[i] = rotated[i] * i;
+    }
+
+    for(int i = 0; i < input.length(); i++)
+    {
+        output += to_string(rotated[i]);
+    }
+
     return output;
 }
 
